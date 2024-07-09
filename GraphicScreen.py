1725a13c1a00c7782abfb6ef1a6c542955654cf7
Anirudh Ravishankar <aravishankar841@student.fuhsd.org>
2024-07-09 15:36:35 -0700

    Yea Im kinda lost

--- GraphicScreen.py
+++ GraphicScreen.py
@@ -1,18 +1,22 @@
 from tkinter import *
 
 root = Tk()
-
+colour_frame = Frame(root)
+options_frame = Frame(root)
 root.title("Welcome to our fracture detection site.")
 lbl = Label(root, text = "What type of fracture do you want to detect")
-lbl.grid()
 
-root.geometry('350x200')
+lbl.grid(column = 500, row = 400)
+
+
+root.geometry('1000x1000')
+
 
 def clicked():
     lbl.configure()
 
 btn = Button(root, text = "Click me" ,
              fg = "red", command = clicked)
-btn.grid(column=2, row=0)
+btn.grid(column=100, row=0)
 root.mainloop()
 
