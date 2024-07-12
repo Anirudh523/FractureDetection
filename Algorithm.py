4d39159de70b194dedaea7dad652e4f21d43d1b5
Anirudh Ravishankar <aravishankar841@student.fuhsd.org>
2024-07-11 21:30:02 -0700

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -24,7 +24,8 @@ from tensorflow.keras import Sequential
 
 accuracy = 0
 
-for _ in range(5):
+for i in range(5):
+    print(f"Iteration {i+1}")
     images_path = r"C:\Users\Anirudh\Documents\GitHub\FractureDetection\Bone_Fracture_Binary_Classification\Bone_Fracture_Binary_Classification\train"  # add path here
     img_labels = ['Fractured', 'Not Fractured']
 
